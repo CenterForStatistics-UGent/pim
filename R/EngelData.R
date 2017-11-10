bad320bb007a0ca2de22d6edee5a7a136b7004b2
Joris Meys <Joris.Meys@UGent.be>
2017-11-10 14:18:01 +0100

    Added description engel data

--- R/EngelData.R
+++ R/EngelData.R
@@ -1,11 +1,20 @@
-#' This is the engel data
+#' Engel's food expenditure data
+#'
+#' This data originates from Koenker and Bassett (1982). It is a regression
+#' data set consisting of 235 observations on income and expenditure on
+#' food for Belgian working class households. The dataset is copied from
+#' the \href{}{quantreg} package.
 #'
 #' @name EngelData
 #' @docType data
-#' @details These are the columns and their meanings
+#'
+#' @format A data frame of 235 observations and 2 variables:
 #' \itemize{
 #' 	\item{\code{foodexp}} Food expenditure (FE)
 #' 	\item{\code{income}} Household income (HI)
 #' }
 #' @keywords data engel
-NULL
+#'
+#' @source Koenker, R. and Bassett, G (1982) Robust Tests of Heteroscedasticity
+#' based on Regression Quantiles; \emph{Econometrica 50, 43-61}.
+'EngelData'
