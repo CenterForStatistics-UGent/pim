404483bf976b6849ff39899f2ffbeb93fbb91918
Joris Meys <Joris.Meys@UGent.be>
2017-11-10 11:59:28 +0100

    Solving issue #9 : coef returns matrix now

--- R/coef.R
+++ R/coef.R
@@ -1,20 +1,22 @@
 #' Extract the coefficients from a pim or pim.summary object
-#' 
+#'
 #' This function works like \code{\link[stats]{coef}} from the \code{stats}
 #' package. It extracts the coefficients from the objects.
-#' 
+#'
 #' @param object a \code{pim} or \code{pim.summary} object
 #' @param ... currently ignored.
-#' 
-#' @return a named vector with the coefficients. 
-#' 
-#' @examples 
+#'
+#' @return a named vector with the coefficients. Or, in case of a
+#' \code{pim.summary} object, a matrix with the coefficients, standard
+#' errors, z values and probabilities.
+#'
+#' @examples
 #' data("FEVData")
 #' Model <- pim(FEV~ Age + Smoke*Sex , data=FEVData)
 #' coef(Model)
 #' summ <- summary(Model)
 #' coef(summ)
-#' 
+#'
 #' @rdname coef
 #' @include pim-class.R pim.summary-class.R
 #' @export
@@ -22,8 +24,20 @@
 
 setGeneric('coef')
 
-coef.pim <- coef.pim.summary <- function(object,...){
+coef.pim <- function(object, ...){
   object@coef
+}
+
+coef.pim.summary <- function(object,...){
+  out <- cbind(object@coef,
+               object@se,
+               object@zval,
+               object@pr)
+  colnames(out) <- c("Estimate",
+                     "Std. Error",
+                     "z value",
+                     "Pr(>|z|)")
+  out
 }
 
 #' @rdname coef
