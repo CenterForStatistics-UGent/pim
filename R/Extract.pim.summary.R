da225fe639180edb8cd6ac1f128fe9170b41d611
Joris Meys <Joris.Meys@UGent.be>
2018-01-26 12:07:52 +0100

    Adapted extraction to mimick matrix better + updated help files

--- R/Extract.pim.summary.R
+++ R/Extract.pim.summary.R
@@ -1,21 +1,23 @@
 #' Extract method for pim.summary objects
-#' 
+#'
 #' This method allows to extract data directly from a \code{\link{pim.summary}}
 #' object. It's exactly the same as extracting from \code{as.matrix(thesummary)}.
-#' 
+#'
 #' @inheritParams base::Extract
 #' @param j see i
-#' 
+#'
 #' @return the selected matrix
-#' 
-#' @examples 
+#'
+#' @seealso \code{\link{as.matrix.pim.summary}} for conversion to a matrix
+#'
+#' @examples
 #' data(FEVData)
 #' Model <- pim(FEV~ Smoke*Sex , data=FEVData)
-#' 
+#'
 #' thesummary <- summary(Model)
 #' thesummary[,2:3]
-#' thesummary["Sex"]
-#' 
+#' thesummary["Sex",]
+#'
 #' @docType methods
 #' @include pim.summary-class.R
 #' @rdname Extract.pim.summary
@@ -25,5 +27,10 @@
 setMethod("[",
           "pim.summary",
           function(x,i,j,drop = TRUE){
-            as.matrix(x)[i,j,drop = drop]
+            if(missing(j)){
+              as.matrix(x)[i,drop = drop]
+            } else {
+              as.matrix(x)[i,j,drop = drop]
+            }
+
           })
--- R/as.matrix.pim.summary.R
+++ R/as.matrix.pim.summary.R
@@ -1,15 +1,27 @@
 #' Convert a pim.summary object to a matrix
-#' 
+#'
 #' This function converts a summary object to a matrix so values can be
-#' extracted from it.
-#' 
+#' extracted from it. This function is provided mainly for internal use.
+#' One can extract immediately from a \code{\link{pim.summary}} object
+#' using standard matrix extraction.
+#'
 #' @param x a \code{pim.summary} object
 #' @param ... additional arguments to be passed to or from methods.
 #' This one is ignored.
-#' 
+#'
 #' @return a matrix with the estimate, standard error, Z value and
-#' probability for every coefficient. 
-#' 
+#' probability for every coefficient.
+#'
+#' @seealso \code{\link{Extract.pim.summary}} for more information on
+#' extracting from a \code{pim.summary} object.
+#'
+#' @examples
+#' data(FEVData)
+#' Model <- pim(FEV~ Smoke*Sex , data=FEVData)
+#'
+#' thesummary <- summary(Model)
+#' as.matrix(thesummary)
+#'
 #' @include pim.summary-class.R
 #' @rdname as.matrix.pim.summary
 #' @name as.matrix.pim.summary
