fee6daf50e70fe9de57c233ed34b1a8fa0af78c4
Joris Meys <Joris.Meys@UGent.be>
2018-01-26 10:34:13 +0100

    adapted help page MHData

--- R/MHData.R
+++ R/MHData.R
@@ -1,12 +1,23 @@
-#' This is the Mental health data
+#' Mental Health in Alachua County
+#'
+#' This dataset originates from Agresti (2007) and consists of mental health
+#' data for a random sample of 40 adult residents of Alachua County, Florida.
+#'
+#' @source Agresti, A. (2007) An Introduction to Categorical Data Analysis.
+#' Hoboken: Wiley.
 #'
 #' @name MHData
 #' @docType data
 #' @details These are the columns and their meanings
 #' \itemize{
-#' 	\item{\code{mental}} Mental impairment (MI)
-#' 	\item{\code{ses}} Socioeconomic status
-#' 	\item{\code{life}} Life index (LI)
+#' 	\item{\code{mental}}: Mental impairment (MI) as an ordinal variable with
+#' 	categories 1 (well), 2 (mild symptom formation), 3 (moderate symptom
+#' 	formation) and 4 (impaired)
+#' 	\item{\code{ses}}: Socioeconomic status as a binary variable, coded as
+#' 	0 (low socio-economic status) and 1 (high socio-economic status)
+#' 	\item{\code{life}}: Life index (LI) as a composite measure that
+#' 	quantifies the severity and number of important life events such as
+#' 	birth of a child, death in the family and divorce.
 #' }
-#' @keywords data FEV
-NULL
+#' @keywords data MH MentalHealth
+"MHData"
