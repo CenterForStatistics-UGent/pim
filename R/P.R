510a484cb2107162b12ff361491cfb5d48612bfd
Joris Meys <Joris.Meys@UGent.be>
2018-02-08 16:04:38 +0100

    Added examples to P and PO

--- R/P.R
+++ R/P.R
@@ -1,33 +1,38 @@
 #' Probability function
-#' 
+#'
 #' This functions transform a comparison or otherwise
 #' logical value to a numeric value for use in a pim.
-#' 
-#' These functions are constructed purely for notation. 
+#'
+#' These functions are constructed purely for notation.
 #' \code{P} is completely equivalent
 #' to \code{\link{as.numeric}}, apart from an extra control
-#' to check whether it actually makes sense to do so. 
+#' to check whether it actually makes sense to do so.
 #' The function \code{PO} is just short for \code{P(x < y) + 0.5*P(x == y)}
-#' 
+#'
 #' @param x for \code{P}, a logical value. For \code{PO} a numeric value.
 #' @param y a numeric value or \code{NULL}. If \code{NULL}, the function
 #' will try to calculate \code{PO(L(x),R(x))}, provided the functions
 #' \code{\link{L}} and \code{\link{R}} are defined correctly. This
 #' is the case when \code{PO} is used in the context of a probabilistic
 #' index model fitted with \code{\link{pim}}.
-#' 
-#' @return A numeric value of 0, 0.5 or 1. 1 if x < y, 0.5 if x == y 
+#'
+#' @return A numeric value of 0, 0.5 or 1. 1 if x < y, 0.5 if x == y
 #' and 0 if x > y
-#' 
+#'
 #' @seealso \code{\link{pim}} and \code{\link{pim.formula}} for more information
 #' on how this is used inside a pim context.
+#'
 #' @examples
-#' # Check in pim
-#' 
+#' ## These examples are only for illustration. See the examples in
+#' ## ?pim for how to use these functions in a pim context.
+#' x <- 1:10
+#' P(x > 5)
+#' PO(x, 5)
+#'
 #' @aliases PO
 #' @export
 P <- function(x){
-  if(is.logical(x)) as.numeric(x) else 
+  if(is.logical(x)) as.numeric(x) else
     stop("P requires logical values.")
 }
 
