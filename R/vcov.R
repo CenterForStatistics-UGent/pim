594d7e6607d28556813c0c155caa7bad8eaff4db
Joris Meys <Joris.Meys@UGent.be>
2018-02-08 14:14:19 +0100

    Updated vcov per Duncan's comments

--- R/vcov.R
+++ R/vcov.R
@@ -1,21 +1,23 @@
 #' Methods for vcov
-#' 
-#' This package defines an S4 generic for \code{\link[stats]{vcov}} and methods for list and pim classes.
-#' 
-#' @param object any object. 
+#'
+#' Extract the variance-covariance matrix from a pim model.
+#' This package defines an S4 generic for \code{\link[stats]{vcov}} and methods for list and pim classes. Other than that, \code{vcov} works as described in
+#' \code{stats::\link[stats]{vcov}}.
+#'
+#' @param object any object.
 #' @param ... arguments passed to other methods. Currently ignored
-#' 
-#' @return the variance-covariance matrix
-#' 
-#' @seealso \code{\link[stats]{vcov}} in the stats package.
-#' 
-#' @examples 
+#'
+#' @return A matrix with the variance-covariance values
+#'
+#' @seealso \code{stats::\link[stats]{vcov}} in the stats package.
+#'
+#' @examples
 #' data(FEVData)
 #' Model <- pim(FEV~ Age + Smoke*Sex , data=FEVData)
 #' vcov(Model)
 
 #' @docType methods
-#' @include pim-class.R 
+#' @include pim-class.R
 #' @export
 
 # There is no help page for vcov here, as it behaves as shown on the
