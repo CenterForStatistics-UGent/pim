cbf8f2583b3f833d34a2a4c4ae62f3edbf2d4ec6
Joris Meys <Joris.Meys@UGent.be>
2018-02-09 18:40:34 +0100

    Updated info in pim.poset-class

--- R/pim.poset-class.R
+++ R/pim.poset-class.R
@@ -1,19 +1,19 @@
 #' The pim.poset class
-#' 
+#'
 #' The pim.poset class is an S4 class that inherits from \code{\link{environment}}
 #' and contains the poset functions for a \code{\link{pim}}. It's a class
 #' used internally and should not be adapted by the user. The correct
 #' interpretation of the formula is dependent on this object. The object
-#' mainly functions as a slot in object of class 
+#' mainly functions as a slot in object of class
 #' \code{\link{pim.environment}}.
-#' 
+#'
 #' @slot compare a character value with the type of poset. This can
 #' take the values "unique", "all" and "custom".
-#' @slot nobs an integer value describing the number of observations 
-#' for which this poset is meant to be used. 
-#' 
+#' @slot nobs an integer value describing the number of observations
+#' for which this poset is meant to be used.
+#'
 #' @section Note:
-#' The pim.poset class doesn't really make sense to be used on itself.
+#' The \code{pim.poset} class doesn't really make sense on its own.
 #' It is part of the \code{\link{pim.environment}} class and shouldn't
 #' be used outside this context.
 #' @aliases pim.poset
