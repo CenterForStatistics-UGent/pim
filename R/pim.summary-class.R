8eb844887c5c14ddcc7a856f24953c6b5fb4537d
Joris Meys <Joris.Meys@UGent.be>
2018-02-09 20:09:17 +0100

    Corrected small typo in pim.summary-class

--- R/pim.summary-class.R
+++ R/pim.summary-class.R
@@ -1,17 +1,17 @@
 #' Class pim.summary
-#' 
+#'
 #' This class contains the summary information from a probabilistic index
-#' model , and is created by using the function \code{\link{summary}}
+#' model, and is created by using the function \code{\link{summary}}
 #' on an object of the \code{\link{pim-class}}.
-#' 
+#'
 #' The class \code{pim.summary} can be treated like a matrix to get out
-#' the coefficients, standard errors, Z values and/or p values. 
-#' 
+#' the coefficients, standard errors, Z values and/or p values.
+#'
 #' @slot formula contains an object of the class \code{\link{pim.formula}}
 #' containing the model fitted.
-#' @slot model a character vector describing the type of model. See also the argument \code{model} of the function \code{\link{pim}}
+#' @slot model a character vector describing the type of model. See also the argument \code{model} of the function \code{\link{pim}}.
 #' @slot link a character value that contains the link. See also
-#' the argument \code{link} of the function \code{\link{pim}}
+#' the argument \code{link} of the function \code{\link{pim}}.
 #' @slot coef a numeric vector with the coefficients
 #' @slot se a numeric vector with the standard errors for the coefficients
 #' @slot zval a numeric vector containing the Z values for the coefficients,
@@ -20,13 +20,13 @@
 #' coefficients.
 #' @slot h0 a numeric value or a numeric vector containing
 #' the null hypothesis. See the argument at \code{\link{summary.pim}}
-#' 
+#'
 #' @seealso \code{\link{pim}} for more info on how to construct the model
 #' and \code{\link{summary.pim}} for the constructor.
-#' 
+#'
 #' @include pim-class.R
 #' @aliases pim.summary
-#' 
+#'
 setClass(
   'pim.summary',
   slots=c(formula='pim.formula',
@@ -39,7 +39,7 @@ setClass(
           h0 = 'numeric'
           ),
   validity = function(object){
-    
+
     if(!.equal.lengths(
       object@coef,
       object@se,
@@ -50,7 +50,7 @@ setClass(
     } else {
       TRUE
     }
-       
+
   }
 )
 
