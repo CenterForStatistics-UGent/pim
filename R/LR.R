b0afaf6a6c2dc165afbcd648c457b54ca05475e6
Joris Meys <Joris.Meys@UGent.be>
2018-02-15 18:20:21 +0100

    L(), R() and PP() now return errors when trying to use them outside a pim formula.

--- R/LR.R
+++ R/LR.R
@@ -3,7 +3,7 @@
 #' These functions allow you to specify the left hand side and
 #' right hand side of a term in a pim model. The user should
 #' only use this functions within a formula using the \code{\link{pim}}
-#' function. Use in a different context will return an error.
+#' function.
 #'
 #' @param x any vector specified in a formula
 #'
@@ -15,14 +15,13 @@
 #' @details These specific functions are actually not used by the
 #' function \code{\link{pim}}. \code{pim} calls the internal function
 #' \code{\link{.make.posfun}} to create the actual functions \code{L}
-#' and \code{R} to work with the specified posets of the model of
-#' interest.
+#' and \code{R} in a \code{\link{pim.poset}} object.
 #'
 #' The actual functions used by \code{pim} are saved in a specific environment, a \code{\link{pim.environment}}, which resides in the \code{\link{pim-class}} object returned by \code{pim}. This way of working
 #' is chosen in order to avoid unnecessary copying of data.
 #'
 #' The function \code{PP} serves simply as short for \code{R(x) - L(x)}. If used outside the context of a pim model, it will generate
-#' multiple warnings (see section warning).
+#' errors (see section warning).
 #'
 #' @section warning:
 #'
@@ -44,21 +43,26 @@
 #' @aliases R L PP
 #' @export
 L <- function(x){
-  warning("L() is not correctly defined. Please see ?L for more information.")
-  x
+  stop(paste("L() is not tied to a set of pseudo observations.", "
+          Did you try to use L() outside of a pim context?"))
 }
 
 #' @rdname L
 #' @name L
 #' @export
 R <- function(x){
-  warning("R() is not correctly defined. Please see ?R for more information.")
-  x
+  stop(paste("R() is not tied to a set of pseudo observations.", "
+          Did you try to use R() outside of a pim context?"))
 }
 
 #' @rdname L
 #' @name L
 #' @export
 PP <- function(x){
-  R(x) - L(x)
+  tryCatch(R(x) - L(x),
+           error = function(e) stop(paste(
+             "PP() is not tied to a set of pseudo observations.",
+             "Did you try to use PP() outside of a pim context?"
+           ), call. = FALSE)
+           )
 }
