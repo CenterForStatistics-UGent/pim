f9be325a5f7e4009b33be9ae17d8d7996bcfff14
Joris Meys <Joris.Meys@UGent.be>
2020-02-03 14:57:23 +0100

    Update after comments CRAN

--- R/nobs.R
+++ R/nobs.R
@@ -1,27 +1,26 @@
-#' Extract the number of observations 
-#' 
+#' Extract the number of observations
+#'
 #' This function extracts the number of observations in an object
 #' of class \code{\link{pim.environment}}, or the number of observations
 #' for which a \code{\link{pim.poset}} is constructed. If applied to
-#' a matrix or data.frame, it returns the number of rows. 
-#' For any other object it 
+#' a matrix or data.frame, it returns the number of rows.
+#' For any other object it
 #' does the same as \code{\link{length}}.
-#' 
+#'
 #' This package imports the generic \code{\link[stats]{nobs}} from the package
 #' \code{stats4}.
-#'  
+#'
 #' @param object an object of the class \code{\link{pim.environment}} or \code{\link{pim.poset}}
-#' @param ... arguments passed to other methods.
-#' 
+#'
 #' @return In case the function is called on a \code{pim.environment}
-#' or a \code{pim.poset} object, 
+#' or a \code{pim.poset} object,
 #' an integer with the number of (foreseen) observations. If the
-#' pim.environment is empty, it returns \code{0}. 
-#' 
+#' pim.environment is empty, it returns \code{0}.
+#'
 #' In all other cases, it returns the output of either \code{\link{nrow}} (for
 #' matrices and data.frames)
-#' or \code{\link{length}}. 
-#' 
+#' or \code{\link{length}}.
+#'
 #' @include pim.environment-class.R
 #' @export
 
